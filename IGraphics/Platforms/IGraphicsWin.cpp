08c1d127a0055d820c9449a598a4915a79511392
Oli Larkin <olilarkin@googlemail.com>
2021-04-25 14:33:31 +0200

    Tidy up IGraphicsSkia.cpp

--- IGraphics/Platforms/IGraphicsWin.cpp
+++ IGraphics/Platforms/IGraphicsWin.cpp
@@ -1098,8 +1098,6 @@ void IGraphicsWin::DestroyGPUContext()
 
 void IGraphicsWin::ActivateGLContext()
 {
-  if (GetBackendMode() == EBackendMode::Software) return;
-
 #ifdef IGRAPHICS_GL
   mStartHDC = wglGetCurrentDC();
   mStartHGLRC = wglGetCurrentContext();
@@ -1110,8 +1108,6 @@ void IGraphicsWin::ActivateGLContext()
 
 void IGraphicsWin::DeactivateGLContext()
 {
-  if (GetBackendMode() == EBackendMode::Software) return;
-
 #ifdef IGRAPHICS_GL
   ReleaseDC(mPlugWnd, (HDC) GetPlatformContext());
   wglMakeCurrent(mStartHDC, mStartHGLRC); // return current ctxt to start
